bb5e82d9547d57f4e2a41f68543bd322aed3f005
fhernandezvalderrama <loscoensongenios@gmail.com>
2026-02-24 11:41:13 +0000

    Add files via upload

---
+++ hangman_palabras.py
@@ -0,0 +1,13 @@
+lista_palabras = [
+    'casa',
+    'perro',
+    'coche',
+    'alcachofa',
+    'bote',
+    'murcielago',
+    'edificio',
+    'pantalla',
+    'television',
+    'silla',
+    'cerveza'
+]
